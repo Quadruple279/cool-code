f7ed9896854cf27a48839243d274af4e8b8ec409
Quadruple279 <inyourcrib@gmail.com>
2026-07-15 21:27:54 +0700

    six_dices looks too ugly

--- six_dices.py
+++ six_dices.py
@@ -39,25 +39,32 @@ dices = {
 
 print("DICE ROULETTE".center(100, "="))
 print("")
-print("IF YOU SCORE LOWER THAN 20 -> YOU'RE DEAD".center(100, " "))
-print("")
 
 track_rolls = []
 for i in range(6):
     time.sleep(1)
     print(f"{6 - i} ROLL(S) LEFT".center(100, " "))
+
     roll = random.randint(1, 6)
     track_rolls.append(roll)
+
     for i in range(5):
         print(dices.get(roll)[i].center(100, " "))
+
 time.sleep(1)
+
+
 print(" SUMMARY".center(100, "-"))
 print("")
-print(f"TOTAL SCORE: {sum(track_rolls)}".center(100, " "))
+print(f"TOTAL SCORE: {sum(track_rolls)}".center(100, " ") + "\n")
+
+
 for i in range(5):
     print(f"{dices.get(track_rolls[0])[i]:^16} {dices.get(track_rolls[1])[i]:^16} {dices.get(track_rolls[2])[i]:^16} {dices.get(track_rolls[3])[i]:^16} {dices.get(track_rolls[4])[i]:^16} {dices.get(track_rolls[5])[i]:^16}")
 
+print("")
+
 if sum(track_rolls) < 20:
-    print("YOU'RE TERMINATED".center(100, " "))
+    print("Calamity strikes again.".center(100, " "))
 else:
-    print("Nice".center(100, " "))
+    print("Nice.".center(100, " "))
